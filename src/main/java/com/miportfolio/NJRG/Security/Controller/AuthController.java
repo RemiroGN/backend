d466c5362dedd71eda73a97986772f5c1a65d3d0
Sybarite <Remirogomeznicolas@gmail.com>
2023-05-13 17:38:31 -0300

    Arreglando errores del CORS

--- src/main/java/com/miportfolio/NJRG/Security/Controller/AuthController.java
+++ src/main/java/com/miportfolio/NJRG/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendportofolio.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
