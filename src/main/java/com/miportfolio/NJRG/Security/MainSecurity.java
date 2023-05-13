8970b50ae2f10c4891ccaaa89a8bcba94f399603
Sybarite <Remirogomeznicolas@gmail.com>
2023-05-13 20:37:18 -0300

    Fix

--- src/main/java/com/miportfolio/NJRG/Security/MainSecurity.java
+++ src/main/java/com/miportfolio/NJRG/Security/MainSecurity.java
@@ -2,14 +2,13 @@ package com.miportfolio.NJRG.Security;
 
 import com.miportfolio.NJRG.Security.Service.UserDetailsImpl;
 import com.miportfolio.NJRG.Security.jwt.JwtEntryPoint;
-import com.miportfolio.NJRG.Security.jwt.JwtProvider;
 import com.miportfolio.NJRG.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
